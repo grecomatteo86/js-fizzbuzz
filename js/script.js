3f97d9be381a28aff27de68bb00069144d7c6fc2
Matteo <grecomatteo86@gmail.com>
2021-03-02 16:48:02 +0100

    final commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
 //1. Stampo tutti i numeri da 1 a 100
-for ( var i = 1; i <= 100; i++){
-  console.log(i);
+for ( var i = 1; i <= 100; i++) {
+  /* 2. intercetto i multipli di 3 e di 5 e stampo
+  in cima alle condizioni metto sempre la più specifica
+  */
+  if (i % 3 == 0 && i % 5 == 0){
+    console.log('FizzBuzz');
+    // 3. intercetto i multipli di 3 e stampo
+  } else if (i % 3 == 0) {
+    console.log('Fizz');
+    // 4. intercetto i multipli di 5 e stampo
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+    // 5. stampo il resto dei numeri
+  } else {
+    console.log(i);
+  }
 }
